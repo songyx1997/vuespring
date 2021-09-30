811fb614981b45d4a36dc785249e58e3529bf787
Yunxiang Song <songyunxiang1997@163.com>
2021-09-30 22:17:09 +0800

    登录页面美化

--- vuedemo/src/components/Login.vue
+++ vuedemo/src/components/Login.vue
@@ -1,21 +1,63 @@
 <template>
-  <div>
-    用户名:<input
-      type="text"
-      v-model="loginForm.userName"
-      placeholder="请输入用户名"
-    />
-    <br /><br />
-    密码：
-    <input
-      type="password"
-      v-model="loginForm.userPassword"
-      placeholder="请输入密码"
-    />
-    <br /><br />
-    <button v-on:click="login">登录</button>
-  </div>
+  <body>
+<el-container>
+  <el-header>Header</el-header>
+  <el-main>Main</el-main>
+  <el-footer>Footer</el-footer>
+</el-container>
+
+    <!-- <div class="login-form">
+      <el-form ref="loginForm" :model="form" label-width="0px">
+        <el-form-item>
+          <el-input
+            v-model="loginForm.userName"
+            placeholder="手机号/邮箱/用户名"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            type="password"
+            v-model="loginForm.userPassword"
+            placeholder="密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="login-button" type="primary" @click="login"
+            >登录</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div> -->
+  </body>
 </template>
+<style>
+body {
+  /* 覆盖掉浏览器样式 */
+  margin: 0px;
+}
+.login-background{
+  background: url('../assets/login/background.jpg');
+  /* height: 100%;
+  width: 100%;
+  background-size: cover;
+  position: relative; */
+}
+.login-form {
+  /* 响应式布局 */
+  position: relative;
+  top: 25vh;
+  left: 60vw;
+  width: 25vw;
+  /* 四角变圆 */
+  border-radius: 25px;
+  padding: 2.5em 2.5em 0.5em 2.5em;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+}
+.login-button {
+  width: 100%;
+}
+</style>
 <script>
 export default {
   name: 'Login',
--- vuedemo/src/main.js
+++ vuedemo/src/main.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 // 设置反向代理
 var axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:8001/api'
-// 全局注册，之后可在其他组件中通过 this.$axios 发送数据
+// 全局注册
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  render: h => h(App)
 })

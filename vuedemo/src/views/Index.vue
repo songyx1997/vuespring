a95e2d77c14b25403399626b765e0433b52d6b4b
Yunxiang Song <songyunxiang1997@163.com>
2021-10-15 17:30:07 +0800

    首页布局初始化

--- vuedemo/src/views/Index.vue
+++ vuedemo/src/views/Index.vue
@@ -6,7 +6,27 @@
         ><el-header style="height:50px;padding:0px"
           ><nav-menu @navClick="navClick"></nav-menu
         ></el-header>
-        <el-main></el-main>
+        <el-main>
+          <el-row :gutter="20">
+            <el-col :xs="24" :sm="6">
+              <el-card class="box-card"> </el-card>
+            </el-col>
+            <el-col :xs="24" :sm="18">
+              <el-tabs v-model="activeName" type="border-card">
+                <el-tab-pane name="timeline">
+                  <span slot="label"
+                    ><i class="el-icon-s-promotion"></i>&nbsp;时间线</span
+                  >
+                </el-tab-pane>
+                <el-tab-pane name="account">
+                  <span slot="label"
+                    ><i class="el-icon-user-solid"></i>&nbsp;个人中心</span
+                  >
+                </el-tab-pane>
+              </el-tabs>
+            </el-col>
+          </el-row>
+        </el-main>
       </el-container>
     </el-container>
   </body>
@@ -20,6 +40,11 @@ export default {
   components: {
     NavMenu,
     ScrollMenu
+  },
+  data () {
+    return {
+      activeName: 'account'
+    }
   },
   methods: {
     navClick () {

70b83db3126ffa8e48e9aa757042fdac6f59d4e1
Yunxiang Song <songyunxiang1997@163.com>
2021-10-18 16:28:10 +0800

    初始化用户卡片

--- vuedemo/src/components/UserInfoCard.vue
+++ vuedemo/src/components/UserInfoCard.vue
@@ -1,12 +1,30 @@
 <template>
-  <el-card> </el-card>
+  <el-card>
+    <div class="card-avatar">
+      <el-avatar :size="100" :src="user.avatarUrl"></el-avatar>
+      <div class="card-name">{{ user.name }}</div>
+      <div class="card-identity">{{ user.identity }}</div>
+    </div>
+  </el-card>
 </template>
-<style scoped></style>
+<style scoped>
+.card-avatar {
+  padding-top: 4px;
+  border-radius: 10px;
+}
+</style>
 <script>
+import avatar from '@/assets/avatar.gif'
 export default {
   name: 'UserInfoCard',
   data () {
-    return {}
+    return {
+      user: {
+        name: 'songyx',
+        identity: 'admin',
+        avatarUrl: avatar
+      }
+    }
   }
 }
 </script>
